db84885c11d84eccd24db6f92cf8b8fda7f9b17e
softwareengineerfs <softwareengineerfs@gmail.com>
2024-03-26 10:30:33 +0500

    add about page

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const elsBtn = document.querySelectorAll(".director__plus");
+
+elsBtn.forEach((btn)=> {
+ btn.addEventListener("click", function () {
+
+  // add activeness to button and its icon
+  btn.classList.toggle("director__plus-active");
+  btn.children[0].classList.toggle("director__buttom-icon-active");
+
+  // change information
+  btn.previousElementSibling.classList.toggle("open")
+  btn.previousElementSibling.previousElementSibling.classList.toggle("close")
+
+
+  btn.parentElement.style.backgroundColor = "#002529";
+
+ });
+});
